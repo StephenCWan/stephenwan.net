c48975b7e9aafd922c46055567770f9e50f9c6e1
Stephen Wan <stephen@stephenwan.net>
2013-06-22 18:27:46 -0500

    fixing port setup

--- webserver/server.js
+++ webserver/server.js
@@ -5,6 +5,8 @@ var path = require("path");
 var port = process.env.PORT | 8000;
 
 app.use(express.static(path.join(__dirname + "/../app")));
+app.use(express.query());
+app.use(express.cookieParser());
 
 app.listen(port, "0.0.0.0", function() {
 	console.log("Launching stephenwan.net on port " + port);
