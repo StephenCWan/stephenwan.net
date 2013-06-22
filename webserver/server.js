f0b271b738dff06c84aa3d53825289b81ea6905c
Stephen Wan <stephen@stephenwan.net>
2013-06-22 17:56:30 -0500

    fixed port field

--- webserver/server.js
+++ webserver/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-var port = process.env.port | 8000;
+var port = process.env.PORT | 8000;
 
 app.use(express.static(path.join(__dirname + "/../app")));
 
