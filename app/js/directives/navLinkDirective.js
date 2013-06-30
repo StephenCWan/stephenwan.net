cc08d36d71511fc154f0f62c9af2ae4c2f501a1c
Stephen Wan <stephen@stephenwan.net>
2013-06-29 22:59:25 -0500

    implemented navigation links as directives http://docs.angularjs.org/guide/directive

---
+++ app/js/directives/navLinkDirective.js
@@ -0,0 +1,19 @@
+var module = angular.module('stephenwan.directives', []);
+
+module.directive('navLink', [function() {
+	return {
+		restrict: "E",
+		replace: true,
+		templateUrl: "partials/directives/navLink.html",
+		controller: function($scope, $attrs, $http) {
+			$http({ method: "GET", url: $attrs.iconRef }).success(function(data) {
+				$scope.icon = data;
+			});
+		},
+		scope: {
+			title: "@title",
+			iconRef: "@iconRef",
+			link: "@link"
+		}
+	}
+}]);
