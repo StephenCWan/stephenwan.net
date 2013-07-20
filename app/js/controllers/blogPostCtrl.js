7d0caad004f57b0a69c67fb8da360d406b9a02da
Stephen Wan <stephen@stephenwan.net>
2013-07-20 01:48:04 -0500

    moving to require.js

--- app/js/controllers/blogPostCtrl.js
+++ app/js/controllers/blogPostCtrl.js
@@ -1,4 +1,8 @@
-angular.module('stephenwan.controllers')
-.controller('blogPostCtrl', ['$scope', function($scope) {
-	
-}])
+define(['angular', '../controllers'], function(angular) {
+
+
+	angular.module('stephenwan.controllers')
+	.controller('blogPostCtrl', ['$scope', function($scope) {
+
+	}]);
+});
--- app/js/controllers/contactCtrl.js
+++ app/js/controllers/contactCtrl.js
@@ -1,14 +1,18 @@
-angular.module('stephenwan.controllers')
-.controller('contactCtrl', ['$scope', '$http', function($scope, $http) {
-	$scope.sendMessage = function() {
-		$http({ url: '/api/contact', method: "POST", data: { 
-			'message': $scope.message,
-			'name': $scope.name,
-			'email': $scope.email
-		}}).success(function(data, status) {
-			
-		}).error(function(data, status) {
+define(['angular', '../controllers'], function(angular) {
 
-		});
-	};
-}]);
+	angular.module('stephenwan.controllers')
+	.controller('contactCtrl', ['$scope', '$http', function($scope, $http) {
+		$scope.sendMessage = function() {
+			$http({ url: '/api/contact', method: "POST", data: { 
+				'message': $scope.message,
+				'name': $scope.name,
+				'email': $scope.email
+			}}).success(function(data, status) {
+
+			}).error(function(data, status) {
+
+			});
+		};
+	}]);
+
+});
